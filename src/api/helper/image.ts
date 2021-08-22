72d8534d267df8abc15c3e87905aeb1e3ffcaead
ymkz <ymkz@users.noreply.github.com>
2021-08-23 05:01:53 +0900

    chore: update resource target to clip

--- src/api/helper/image.ts
+++ src/api/helper/image.ts
@@ -3,7 +3,7 @@ import { PageData } from '~/types/page-data'
 
 export async function uploadImage(pageData: PageData): Promise<string> {
   const endpoint = 'https://api.cloudinary.com/v1_1/ymkz/image/upload'
-  const params = `?upload_preset=readlater&public_id=${pageData.id}&file=${pageData.imageUrl}`
+  const params = `?upload_preset=clip&public_id=${pageData.id}&file=${pageData.imageUrl}`
   const response = await fetch(`${endpoint}${params}`, { method: 'POST' })
   const { secure_url } = await response.json()
   return secure_url
@@ -12,7 +12,7 @@ export async function uploadImage(pageData: PageData): Promise<string> {
 export async function removeImage(id: PageData['id']): Promise<void> {
   const endpoint = 'https://api.cloudinary.com/v1_1/ymkz/image/destroy'
   const timestamp = Date.now().toString()
-  const base = `invalidate=true&public_id=readlater/${id}&timestamp=${timestamp}`
+  const base = `invalidate=true&public_id=clip/${id}&timestamp=${timestamp}`
   const msg = `${base}${CLOUDINARY_API_SECRET}`
   const signature = await crypto.subtle.digest('SHA-1', encode(msg)).then(toHEX)
   const params = `?${base}&signature=${signature}&api_key=${CLOUDINARY_API_KEY}`
