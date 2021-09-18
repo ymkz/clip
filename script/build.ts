d1fb3616a9bb6d6279b6d59c49c1bb6cab9a1a4a
ymkz <ymkz@users.noreply.github.com>
2021-09-18 22:44:11 +0900

    chore: fix entry-point

--- script/build.ts
+++ script/build.ts
@@ -4,24 +4,24 @@ import esbuild from 'esbuild'
 import fs from 'fs-extra'
 
 const run = async () => {
-  await Promise.all([fs.remove('./dist-backend'), fs.remove('./dist-frontend')])
+  await Promise.all([fs.remove('dist-backend'), fs.remove('dist-frontend')])
   await Promise.all([
     esbuild.build({
       plugins: [paths({})],
       bundle: true,
-      outdir: './dist-backend',
-      entryPoints: ['./src/backend/index.ts'],
+      outdir: 'dist-backend',
+      entryPoints: ['src/backend/index.ts'],
     }),
     esbuild.build({
       plugins: [paths({}), html({})],
       bundle: true,
-      outdir: './dist-frontend',
-      entryPoints: ['./src/frontend/index.html'],
+      outdir: 'dist-frontend',
+      entryPoints: ['src/frontend/index.html'],
     }),
   ])
   await Promise.all([
-    fs.copy('./src/frontend/robots.txt', './dist-frontend/robots.txt'),
-    fs.copy('./src/frontend/assets', './dist-frontend/assets'),
+    fs.copy('src/frontend/robots.txt', 'dist-frontend/robots.txt'),
+    fs.copy('src/frontend/assets', 'dist-frontend/assets'),
   ])
 }
 
