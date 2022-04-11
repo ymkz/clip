4905a0e70576dbba1db1d46c7db8e6f0ef642849
Kazuya Yamashita <ymkz@users.noreply.github.com>
2022-04-11 23:45:34 +0900

    fix(deps): update react monorepo to v18 (major) (#113)
    * fix(deps): update react monorepo to v18 (major)
    
    * chore: move react18 render

--- src/render.tsx
+++ src/render.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { SWRConfig } from 'swr'
 import { ClipList } from '~/view/clip-list'
 
@@ -16,4 +16,8 @@ function App() {
   )
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const container = document.querySelector('#root')
+
+if (container) {
+  createRoot(container).render(<App />)
+}
