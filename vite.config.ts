991e602d911d94201b28d0166889deb17b88bd36
ymkz <ymkz@users.noreply.github.com>
2023-02-04 17:02:10 +0000

    chore: move client root

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
-  publicDir: 'src/public',
+  root: 'src/client',
+  build: {
+    outDir: '../../dist',
+    emptyOutDir: true,
+  },
 })
